6ec9a38b50ec6a2170d49f1fee93e9873dd6c8a5
harshraijiwala <harshraijiwala@gmail.com>
2021-08-14 19:10:55 -0700

    Update cpu_utilization_in_percent.sh

--- Bash-scripts/cpu_utilization_in_percent.sh
+++ Bash-scripts/cpu_utilization_in_percent.sh
@@ -5,13 +5,13 @@
 
 #----------------------------------------------------------------------------------------------------
 #****Using uptime and greping the required value to obtain fifth minute load average*****************
-fifth_min_load_avg=`uptime | cut -d',' -f5`
+fifth_min_load_avg="$(uptime | cut -d',' -f5)"
 #-----------------------------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------------------------------
 #Getting the number of cpu cores from /proc/cpuinfo file
 #Diving the load average for 5th minute from the number of cpu cores to obtain the CPU utilizion percent
-cpu_cores=`cat /proc/cpuinfo | grep -e 'cpu cores' | awk -F':' '{print $2}'`
+cpu_cores="$(cat /proc/cpuinfo | grep -e 'cpu cores' | awk -F':' '{print $2}')"
 
 cpu_utilization="$(echo "${fifth_min_load_avg} * 100" | bc)"
 
@@ -20,7 +20,7 @@ percent_cpu_utilization="$(echo "$cpu_utilization / $cpu_cores" | bc)"
 
 #--------------------------------------------------------------------------------------------------------
 #***Sending an alert mail when the CPU Utilization is more than a certain value**************************
-if [ $percent_cpu_utilization -ge 1 ]
+if [ "$percent_cpu_utilization" -ge 1 ]
 then
         echo "Current CPU Utilization in % - $percent_cpu_utilization" | mutt -s "Alert:CPU Utilization Critical" -- hraijiwala@gmail.com
 
